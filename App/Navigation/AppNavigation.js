b1d04b2f3a6da984a7b98bc82726918965758934
vigle7 <vigle7@gmail.com>
2018-06-22 17:58:19 +0800

    暫存

--- App/Navigation/AppNavigation.js
+++ App/Navigation/AppNavigation.js
@@ -1,18 +1,20 @@
 import { StackNavigator } from 'react-navigation'
+import Storage from '../Containers/Storage'
 import LaunchScreen from '../Containers/LaunchScreen'
 
 import styles from './Styles/NavigationStyles'
 
 // Manifest of possible screens
 const PrimaryNav = StackNavigator({
-  LaunchScreen: { screen: LaunchScreen }
+  Storage: { screen: Storage },
+  LaunchScreen: { screen: LaunchScreen },
 }, {
-  // Default config for all screens
-  headerMode: 'none',
-  initialRouteName: 'LaunchScreen',
-  navigationOptions: {
-    headerStyle: styles.header
-  }
-})
+    // Default config for all screens
+    headerMode: 'none',
+    initialRouteName: 'LaunchScreen',
+    navigationOptions: {
+      headerStyle: styles.header,
+    },
+  })
 
 export default PrimaryNav
